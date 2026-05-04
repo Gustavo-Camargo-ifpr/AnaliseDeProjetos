0f3d783ae0283ca4b2104d214c71ade79f43d757
Gustavo Camargo <gustavocamargo203@gmail.com>
2026-05-04 20:29:11 -0300

    Atualizacao automatica

--- src/Sistema.java
+++ src/Sistema.java
@@ -1,21 +1,21 @@
-public class Sistema{
+// public class Sistema{
 
-    private Usuario usuario;
+//     private Usuario usuario;
 
-    public void inicializaUsuario(Usuario usuario){
-        this.usuario = usuario;
-    }
+//     public void inicializaUsuario(Usuario usuario){
+//         this.usuario = usuario;
+//     }
 
-    public void criarUsuario(String cpf, String nome, String email, String senha){
-        Usuario usuario = new Usuario(cpf, nome, email, senha);
-        this.usuario = usuario;
-        thisusuario.setCpf(cpf);
-        thisusuario.setCpf(nome);
-        thisusuario.setCpf(email);
-        thisusuario.setCpf(senha);
-    }
+//     public void criarUsuario(String cpf, String nome, String email, String senha){
+//         Usuario usuario = new Usuario(cpf, nome, email, senha);
+//         this.usuario = usuario;
+//         thisusuario.setCpf(cpf);
+//         thisusuario.setCpf(nome);
+//         thisusuario.setCpf(email);
+//         thisusuario.setCpf(senha);
+//     }
 
-    public void boaNoitePersolizada(){
-        System.out.println("Boa noite, " + usuario.getNome() + "!");
-    }
-}
+//     public void boaNoitePersolizada(){
+//         System.out.println("Boa noite, " + usuario.getNome() + "!");
+//     }
+// }
